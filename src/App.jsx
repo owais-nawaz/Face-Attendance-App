9bce19bc5255d586cf9d500404a46cf1ff7db7ae
Syed Owais Nawaz <owaisnawaz17@gmail.com>
2024-07-14 17:41:15 +0300

    integrate cameras and buttons

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainComponent from "./MainComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
